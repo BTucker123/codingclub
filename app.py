8d6b1a80d0dfb9497b0bf29eb03aafa1abac1241
Beau <bbaxdav@gmail.com>
2025-12-06 19:52:55 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,181 @@
+from flask import Flask, render_template, url_for, request, redirect, session
+app = Flask(__name__)
+from better_profanity import profanity
+import json
+from date import cleanDate, year, day, month
+
+app.config["SESSION_PERMANENT"] = True
+
+
+def load(file):
+   with open("./data/" + file + '.json', 'r') as f:
+       return json.load(f)
+
+def save(data, file) -> None:
+   with open("./data/" + file + '.json', 'w') as f:
+       json.dump(data, f, indent=4)
+
+siteData = load("site")
+rosterData = load("roster")
+bullitenData = load("bulliten")
+postsData: list = load('posts')
+
+app.secret_key = 'catbean'
+
+
+@app.route("/")
+def homepage():
+    siteData = load("site")
+    rosterData = load("roster")
+    bullitenData = load("bulliten")
+    error = request.args.get("error")
+    return render_template("index.html", error=error, roster=rosterData, bulliten=bullitenData, leader=siteData['leader'])
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.clear()
+    return redirect(url_for("homepage", error="Logged Out."))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    siteData = load("site")
+    rosterData = load("roster")
+    bullitenData = load("bulliten")
+    postsData: list = load('posts')
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        name = request.form.get('username')
+        key = request.form.get('key')
+        if name in rosterData[0] or name in rosterData[1] or name in rosterData[2]:
+            if key == app.secret_key:
+                session['name'] = name
+                if name in rosterData[0]:
+                    session['admin'] = True
+                return redirect(url_for("homepage", error="logged in!"))
+            return render_template("login.html", error="that's not the right key ):<")
+        else:
+            return render_template("login.html", error="....you're not in coding club? (you didn't put in the right name)")
+    return "uhh... beau messed up the coding"
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    siteData = load("site")
+    rosterData = load("roster")
+    bullitenData = load("bulliten")
+    postsData: list = load('posts')
+    if request.method == "GET":
+        if "name" not in session:
+            return redirect(url_for("homepage", error="Unauthorized."))
+        if session["admin"]:
+            return render_template("admin.html", roster=rosterData)
+        return redirect(url_for("homepage", error="Unauthorized."))
+    if request.method == "POST":
+        if request.form.get('type') == "addUser":
+            name = request.form.get('name')
+            rank = request.form.get('rank')
+            d = load('roster')
+            d[int(rank)].append(name)
+            save(d, 'roster')
+            return redirect(url_for("admin"))
+        if request.form.get('type') == "deleteUser":
+            name = request.form.get('name')
+            rank = request.form.get('rank')
+            d:list = load('roster')
+            i = d[int(rank)].index(name)
+            del d[int(rank)][i]
+            save(d, 'roster')
+            return redirect(url_for("admin"))
+        if request.form.get('type') == "addResc":
+            url = request.form.get('url')
+            title = request.form.get('title')
+            rescou:list = load("resc")
+            rescou.append({
+                "url": url,
+                "title": title
+            })
+            save(rescou, "resc")
+            return redirect(url_for("admin"))
+        
+@app.route("/forum", methods=['GET', 'POST'])
+def forum():
+    postsData = load("posts")
+    if request.method == "GET":
+        postsData = load("posts")
+        if "name" not in session:
+            return redirect("/login")
+        return render_template("forum.html", posts=postsData)
+    if request.method == "POST":
+        fType = request.form.get('type')
+        name = session["name"]
+        content = request.form.get('content')
+        date = cleanDate()
+        if fType == "deletePost":
+            id = request.form.get('id')
+            del postsData[int(id)]
+            save(postsData, "posts")
+            return redirect(url_for("forum"))
+        if fType == "newPost":
+            content = request.form.get('content')
+            date = cleanDate() + " '" + str(year)[:2]
+            content = profanity.censor(content)
+            postsData.append({
+                "author": name,
+                "date": date,
+                "content": content,
+                "id": len(postsData),  # append to end so id matches index
+                "replies": []
+            })
+
+            save(postsData, "posts")
+            return redirect(url_for("forum"))
+        if fType == "replyToPost":
+            postId = request.form.get('postID')
+            content = request.form.get('c')
+            date = cleanDate()
+            content = profanity.censor(content)
+            d = load('posts')
+            d[int(postId)]["replies"].append({
+                "author": name,
+                "date": date,
+                "content": content
+            })
+            save(d, "posts")
+            return redirect(url_for("forum"))
+        
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+    postsData = load("chat")
+    if request.method == "GET":
+        postsData = load("chat")
+        if "name" not in session:
+            return redirect("/login")
+        return render_template("chat.html", posts=postsData)
+    if request.method == "POST":
+        fType = request.form.get('type')
+        name = session["name"]
+        content = request.form.get('content')
+        date = cleanDate()
+        if fType == "newPost":
+            content = request.form.get('content')
+            date = cleanDate()
+            content = profanity.censor(content)
+            postsData.append({
+                "author": name,
+                "date": str(month) + "/" + str(day),
+                "content": content,
+                "id": len(postsData)  # append to end so id matches index
+            })
+            save(postsData, "chat")
+            return redirect(url_for("chat"))
+
+@app.route("/rescources")
+def rescources():
+    res = load("resc")
+    return render_template("resc.html", resc=res) 
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host="0.0.0.0")
+
---
+++ date.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def get_day_suffix(day):
+    if 11 <= day <= 13:
+        return "th"
+    return {1: "st", 2: "nd", 3: "rd"}.get(day % 10, "th")
+
+now = datetime.now()
+day = now.day
+year = now.year
+month = now.month
+
+def cleanDate() -> str:
+    now = datetime.now()
+    day = now.day
+    formatted_date = now.strftime(f"%A, %B {day}{get_day_suffix(day)}")
+    return formatted_date
+
+if __name__ == "__main__":
+    print(cleanDate())
